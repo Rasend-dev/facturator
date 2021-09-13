d83d02010371c56025d3b86649bcd777f66e0242
Andrew Molina <rasend20@gmail.com>
2021-09-13 15:41:52 -0700

    logrando que el scraper funcione cuando se unde el boton en streamlit

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import subprocess
 
-st.title('Facturadora')
+st.title('Guía de ventas')
+#this verifies if someone pressed the button
+if(st.button('Calcular tasa del día')):
+    subprocess.run(['py','run.py'], cwd='./Scraper/Scraper/spiders')
+    
+st.sidebar.header('User Input Features')
