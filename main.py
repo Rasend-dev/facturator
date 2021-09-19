8c1c395c5c97b8a491162aafb313781aab13ffaa
Andrew Molina <rasend20@gmail.com>
2021-09-19 08:47:33 -0700

    logrando que los botones se identifiquen

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ PATH_JSON = os.path.join(os.path.join(os.getcwd(),'export'),'json_files')
 root = Tk()
 root.title('Facturacion')
 
+prueba_botones = {'Sushi rico': 8999,'Sushi sabroso':3567,'Sushi extra rico':6543,'Teriyaki':4555,'Monas chinas':
+                    5444,'pulpo a la braza':4555,'orlando sabroso':10000}
+
 def _show_currency():
     subprocess.run(['py','run.py'],cwd='./Scraper/Scraper/spiders')
     with open(os.path.join(PATH_JSON,'mean_currencies.json'), 'r+') as f:
@@ -17,40 +20,46 @@ def _show_currency():
     label_currencies.grid(row=0,column=1)
     button_slide['state'] = DISABLED
 
-def _load_principal_dishes():
+
+def _charge(name):
+    print(name)
+
+def _load_hero(dicti):
     """
     A function that create buttons for the hero part
     """
-    for i in range(10):
-        platillo = Button(hero,text=f'Platillo numero {i + 1}',padx=30,pady=30)
-        platillo.grid(row=3,column=i,padx=20)
-        if i > 4:
-            platillo.grid(row=5,column=i - 5,padx=20)
-    pass
+    platillo = []
+    for i in dicti.keys():
+        platillo.append(Button(hero,text=i,padx=30,pady=30,command=lambda i=i: _charge(i))) #usando i=i en nuesta funcion lambda hacemos que el valor en el que esta el for se guarde
 
-def _load_price_label():
+    for i, name in enumerate(platillo):
+        platillo[i].grid(row=4,column=i,padx=20)
+        if i >4:
+            platillo[i].grid(row=6,column=i -5, padx=20)
+    
+
+def _load_price_label(dicti):
     """
-    a function that 
+    a function that load the price of each dish in the hero
     """
-    for i in range(10):
-        mitad = 10 // 5
-        price = Label(hero,text='Precio: 123123$')
-        price.grid(row=mitad,column=i)
-        if i > 4:
-            price.grid(row=mitad + 2,column=i-5)
+    for idx, price in enumerate(dicti.values()):
+        price = Label(hero,text=f'{price}$')
+        price.grid(row=3,column=idx)
+        if idx > 4:
+            price.grid(row=5,column=idx-5)
         
 
 #slide side
 slide = LabelFrame(root,text='Export zone',padx=20,pady=200)
-slide.grid(row=0,column=0)
+slide.grid(row=0,column=0,sticky='n')
 
 #principal side
 hero = LabelFrame(root,text='Este es la parte principal',pady=120)
-hero.grid(row=0,column=1)
+hero.grid(row=0,column=1,sticky='n')
 
 #sum side
-sum_side = LabelFrame(root,text='esto es una prueba',padx=450,pady=50)
-sum_side.grid(row=1,column=1)
+sum_side = LabelFrame(root,text='esto es una prueba',padx=300,pady=50)
+sum_side.grid(row=1,column=1,sticky='s')
 
 #Slider part
 button_slide = Button(slide,text='Extract todays currency',padx=10,pady=10,command=_show_currency)
@@ -59,14 +68,15 @@ button_slide.grid(row=0,column=0,pady=10)
 button_register.grid(row=1,column=0)
 
 #Hero part
-hero_currencie = Label(hero,text='Current currency value:')
-hero_label = Label(hero,text = 'Platillos disponibles: \n')
-hero_currencie.grid(row=0,column=0)
-hero_label.grid(row=1,column=0)
+value = StringVar()
+change_menu = OptionMenu(hero,value,'Principal','Entradas','Bebidas','Tempurizados','Adicionales')
+hero_currencies = Label(hero,text='Current currency value: ')
+hero_currencies.grid(row=1,column=0)
+change_menu.grid(row=2,column=0,pady=5)
 
-_load_principal_dishes()
+_load_hero(prueba_botones)
 
-_load_price_label()
+_load_price_label(prueba_botones)
 
 
 sum_button = Button(sum_side,text='prueba',padx=10,pady=10)
